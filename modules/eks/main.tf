e0a9ee0ec45747e1b36fbea1d71b016a7595cf9d
Veerendra1233 <veerendra2koncha@gmail.com>
2026-02-06 16:35:22 +0530

    Create main.tf-eks

---
+++ modules/eks/main.tf
@@ -0,0 +1,65 @@
+# 1. EKS Cluster
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.cluster.arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+}
+
+# 2. IAM Role for Cluster
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster_name}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "eks.amazonaws.com" } }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+# 3. Worker Node Group
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "worker-nodes"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.desired_capacity + 1
+    min_size     = 1
+  }
+
+  instance_types = [var.instance_type]
+}
+
+# 4. IAM Role for Nodes
+resource "aws_iam_role" "node_role" {
+  name = "${var.cluster_name}-node-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_role.name
+}
+
+output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
